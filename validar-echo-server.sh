3ad75d7576f7eee3eb1dca24d4a08c069b44d3d6
Francisco Viñas <fvinas@fi.uba.ar>
2024-09-01 19:53:17 -0300

    remove --build flag from validar-echo-server.sh

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker compose -f nc/docker-compose.yaml up --build 2>/dev/null | grep "action" | cut -d'|' -f2- | sed 's/^ //'
+docker compose -f nc/docker-compose.yaml up 2>/dev/null | grep "action" | cut -d'|' -f2- | sed 's/^ //'
