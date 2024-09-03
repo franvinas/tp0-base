7be8a0ead9753810f7c208cf9101a2b2c9c8a07a
Francisco Viñas <fvinas@fi.uba.ar>
2024-09-02 23:37:15 -0300

    handle graceful shutdown of the client when receiving a signal

--- client/common/client.go
+++ client/common/client.go
@@ -4,6 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/op/go-logging"
@@ -23,6 +26,8 @@ type ClientConfig struct {
 type Client struct {
 	config ClientConfig
 	conn   net.Conn
+	running bool
+	stop	chan bool
 }
 
 // NewClient Initializes a new client receiving the configuration
@@ -30,6 +35,8 @@ type Client struct {
 func NewClient(config ClientConfig) *Client {
 	client := &Client{
 		config: config,
+		running: true,
+		stop: make(chan bool),
 	}
 	return client
 }
@@ -45,6 +52,7 @@ func (c *Client) createClientSocket() error {
 			c.config.ID,
 			err,
 		)
+		return err
 	}
 	c.conn = conn
 	return nil
@@ -52,11 +60,21 @@ func (c *Client) createClientSocket() error {
 
 // StartClientLoop Send messages to the client until some time threshold is met
 func (c *Client) StartClientLoop() {
+	go c.handleSignals()
+
 	// There is an autoincremental msgID to identify every message sent
 	// Messages if the message amount threshold has not been surpassed
 	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
+		if !c.running {
+			log.Debugf("action: loop_stopped | result: success | client_id: %v", c.config.ID)
+			return
+		}
+
 		// Create the connection the server in every loop iteration. Send an
-		c.createClientSocket()
+		err := c.createClientSocket()
+		if err != nil {
+			return
+		}
 
 		// TODO: Modify the send to avoid short-write
 		fmt.Fprintf(
@@ -82,8 +100,25 @@ func (c *Client) StartClientLoop() {
 		)
 
 		// Wait a time between sending one message and the next one
-		time.Sleep(c.config.LoopPeriod)
+		timer := time.NewTimer(c.config.LoopPeriod)
+		select {
+			case <-timer.C:
+				continue
+			case <-c.stop:
+				log.Debugf("action: loop_stopped | result: success | client_id: %v", c.config.ID)
+				return
+			}
 
 	}
 	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
+}
+
+func (c *Client) handleSignals() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT)
+	sig := <-sigs
+	log.Debugf("action: signal_received | signal: %v | client_id: %v", sig, c.config.ID)
+	c.running = false
+	c.conn.Close()
+	c.stop <- true
 }
